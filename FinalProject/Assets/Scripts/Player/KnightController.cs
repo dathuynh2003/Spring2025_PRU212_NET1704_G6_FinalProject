adb6d80b73007f3842615277ae9980a134cddaf7
Vũ Đạt <150220982+dathuynh2003@users.noreply.github.com>
2025-03-11 22:09:00 +0700

    [ADD] base code and Knight Animation

---
+++ FinalProject/Assets/Scripts/Player/KnightController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class KnightController : MonoBehaviour
+{
+
+    private Animator animator;
+    private Rigidbody2D rb;
+    private float moveSpeed = 5f;
+    private bool isGrounded = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal"); // Lấy input di chuyển
+
+        // Chạy
+        if (move != 0)
+        {
+            animator.SetBool("isRunning", true);
+            transform.Translate(Vector2.right * moveSpeed * move * Time.deltaTime);
+            transform.localScale = new Vector3(Mathf.Sign(move), 1, 1); // Đổi hướng nhân vật
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+
+        // Nhảy
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, 10f);
+            isGrounded = false;
+        }
+
+        // Đánh
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("Attack");
+            animator.SetBool("isAttack", true);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            animator.ResetTrigger("Attack");
+            animator.SetBool("isAttack", false);
+
+        }
+    }
+
+    // Xử lý va chạm
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
---
+++ FinalProject/Assets/Scripts/PolygonColliderAdjuster.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PolygonColliderAdjuster : MonoBehaviour
+{
+    private PolygonCollider2D polyCollider;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        polyCollider = GetComponent<PolygonCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (spriteRenderer != null)
+        {
+            FitColliderToSprite();
+        }
+    }
+
+    void FitColliderToSprite()
+    {
+        if (polyCollider == null || spriteRenderer == null)
+            return;
+
+        // Xóa đường viền cũ
+        polyCollider.pathCount = 0;
+
+        // Lấy đường viền từ sprite
+        Texture2D texture = spriteRenderer.sprite.texture;
+        Vector2[] newPoints = GenerateColliderFromTexture(texture, spriteRenderer.sprite);
+
+        if (newPoints.Length > 0)
+        {
+            polyCollider.pathCount = 1;
+            polyCollider.SetPath(0, newPoints);
+        }
+    }
+
+    Vector2[] GenerateColliderFromTexture(Texture2D texture, Sprite sprite)
+    {
+        // Lấy dữ liệu pixel của sprite
+        Color[] pixels = texture.GetPixels();
+        int width = texture.width;
+        int height = texture.height;
+
+        // Danh sách các điểm cạnh của nhân vật
+        List<Vector2> colliderPoints = new List<Vector2>();
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (pixels[x + y * width].a > 0.1f) // Lấy những pixel có alpha > 10% (không trong suốt)
+                {
+                    Vector2 worldPoint = new Vector2(
+                        (x - width / 2) / sprite.pixelsPerUnit,
+                        (y - height / 2) / sprite.pixelsPerUnit
+                    );
+                    colliderPoints.Add(worldPoint);
+                }
+            }
+        }
+
+        return colliderPoints.ToArray();
+    }
+}
