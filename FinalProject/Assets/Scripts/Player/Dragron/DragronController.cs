7fdf77b17966773aac5f2b5f0388c4f150a31368
LongLQ25 <quanglong3145@gmail.com>
2025-03-17 00:57:29 +0700

    [ADD] add some animator for dragron

--- FinalProject/Assets/Scripts/Player/Dragron/DragronController.cs
+++ FinalProject/Assets/Scripts/Player/Dragron/DragronController.cs
@@ -4,14 +4,20 @@ public class DragronController : MonoBehaviour
 {
     private Animator animator;
     private Rigidbody2D rb;
+    public int maxHealth = 5;
     private float moveSpeed = 5f;
+    private float move;
     private bool isGrounded = true;
 
     public Transform attackPoint;
+    public float attackRadius = 1f;
+    public LayerMask attackLayer;
     public GameObject fireBall;
 
     public float fireRate = 0.2f;
     private float nextFireTime = 0f;
+
+    private bool isDead = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,12 +25,19 @@ public class DragronController : MonoBehaviour
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
 
+        animator.ResetTrigger("Dizzy");
+        animator.SetBool("isDizzy", false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float move = Input.GetAxis("Horizontal"); // Lấy input di chuyển
+        if (maxHealth < 0)
+        {
+            Die();
+        }
+
+        move = Input.GetAxis("Horizontal"); // Lấy input di chuyển
 
         // Chạy
         if (move != 0)
@@ -53,6 +66,24 @@ public class DragronController : MonoBehaviour
             animator.SetTrigger("Attack");
             nextFireTime = Time.time + fireRate;
         }
+
+        // Đánh chuột phải
+        if (Input.GetMouseButtonDown(1))
+        {
+            animator.SetTrigger("Strike");
+        }
+
+        // Hàm test = nhấn T
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Die();
+        }
+
+        // Hàm test = nhấn Y
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            animator.SetTrigger("Hurt");
+        }
     }
 
     // Xử lý va chạm
@@ -79,5 +110,74 @@ public class DragronController : MonoBehaviour
         {
             fireballScript.SetDirection(1); // Bắn sang phải
         }
+
+        Collider2D colliAttack = Physics2D.OverlapCircle(attackPoint.position, attackRadius, attackLayer);
+        Debug.Log(colliAttack);
+        if (colliAttack)
+        {
+            Debug.Log(colliAttack.gameObject.name + " takes dame");
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRadius);
+    }
+
+    public void TakeDame(int dame)
+    {
+        if (isDead) return; // Nếu đã chết thì không nhận sát thương nữa
+
+        maxHealth -= dame;
+
+        animator.SetTrigger("Hurt");
+
+        if (maxHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Die")) return; // Nếu đã chết thì không làm gì nữa
+
+        isDead = true;
+        animator.SetTrigger("Die");
+
+        // Ngừng di chuyển và tấn công
+        moveSpeed = 0;
+        this.enabled = false; // Tắt script để không nhận input nữa
+
+        Debug.Log("Player Die");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap")) // Nếu va chạm với bẫy (chưa làm bẫy)
+        {
+            TriggerDizzyEffect();
+        }
+    }
+
+    void TriggerDizzyEffect()
+    {
+        animator.SetTrigger("Dizzy");
+        animator.SetBool("isDizzy", true);
+        // Tạm thời vô hiệu hóa di chuyển
+        moveSpeed = 0;
+
+        // Sau 3 giây, rồng sẽ hết choáng và có thể di chuyển lại
+        Invoke("RecoverFromDizzy", 3f);
+    }
+
+    void RecoverFromDizzy()
+    {
+        animator.SetBool("isDizzy", false);
+        moveSpeed = 5f; // Trả lại tốc độ di chuyển
     }
 }
